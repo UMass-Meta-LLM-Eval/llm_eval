2415d3655e0945fa0de30ce3b09a96af5865ba56
Kartik Choudhary <kartikchoudh@umass.edu>
2024-02-16 07:38:21 +0000

    prelim benchmk
    Signed-off-by: Kartik Choudhary <kartikchoudh@umass.edu>

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ Project root on unity: `/work/pi_dhruveshpate_umass_edu/grp22/`
 Repo: `/work/pi_dhruveshpate_umass_edu/grp22/src/llm_eval`
 
 Conda env: `/work/pi_dhruveshpate_umass_edu/grp22/conda/envs/llm-eval`
+
+## Setup
+
+Activate a CPU interactive session and load modules:
+
+```bash
+salloc -N 1 -n 1 -p cpu-preempt -t 2:00:00 --mem=8G
+source activate-cpu.sh
+```
+
+Activate a GPU interactive session and load modules:
+
+```bash
+salloc -N 1 -n 1 -p gpu-preempt -t 2:00:00 --mem=8G --gpus=1
+source activate-gpu.sh
+```
+
+For bigger GPU, set the constraint
+
+```bash
+salloc -N 1 -n 1 -p gpu,gpu-preempt -t 2:00:00 --mem=8G --gpus=1 --constraint="[a100|m40|rtx8000]"
+```
--- run_benchmark.py
+++ run_benchmark.py
@@ -1,30 +1,45 @@
 import torch
 import os
-os.environ['HF_HOME'] = '/work/pi_dhruveshpate_umass_edu/grp22/.cache/huggingface'
+from dotenv import load_dotenv
+load_dotenv()
 
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from lm_eval import simple_evaluate
+from lm_eval.models.huggingface import HFLM
 import json
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using device: {device}')
 
-model_name = 'microsoft/phi-2'
-# pipe = pipeline('text-generation', model=model_name, device_map='cuda',
-#     torch_dtype=torch.float16, trust_remote_code=True)
+# Create the HuggingFace model
+model_name = os.getenv('MODEL_NAME')
+if model_name is None:
+    print('No model name found in environment')
+    # model_name = 'microsoft/phi-2'
+    # model_name = 'meta-llama/Llama-2-7b-hf'
+    model_name = 'EleutherAI/pythia-14m'
+
+print(f'Using model: {model_name}')
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, device_map='cuda',
+    torch_dtype=torch.float16, token=os.getenv('HF_TOKEN'),
+    trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Wrap the model in a lm_eval model
+model = HFLM(pretrained=model, tokenizer=tokenizer, batch_size='auto')
 
 print('Running evaluation...')
 result = simple_evaluate(
-    'hf',
-    model_args=f'pretrained={model_name},device_map=cuda,dtype=float16,trust_remote_code=True',
-    tasks='mmlu',
-    # device=device,
-    batch_size='auto',
-    limit=10)
+    model,
+    tasks=['triviaqa', 'nq_open', 'mmlu'],
+    # limit=10
+    )
 print('Evaluation complete.')
 
-with open('result.json', 'w') as f:
+model_name = model_name.replace('/', '__')
+with open(f'result-{model_name}.json', 'w') as f:
     json.dump(result, f, indent=4)
 
-with open('result_scores.json', 'w') as f:
+with open(f'result_scores-{model_name}.json', 'w') as f:
     json.dump(result['results'], f, indent=4)
