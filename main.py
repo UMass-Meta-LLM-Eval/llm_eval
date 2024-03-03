ae135055e5ec58d9e755b42e6037381d1a8a4147
Kartik Choudhary <kartikchoudh@umass.edu>
2024-03-03 22:06:43 +0000

    benchmark pipeline
    Signed-off-by: Kartik Choudhary <kartikchoudh@umass.edu>

--- main.py
+++ main.py
@@ -1,18 +1,77 @@
-# Use a pipeline as a high-level helper
-import torch
-import os
-os.environ['HF_HOME'] = '/work/pi_dhruveshpate_umass_edu/grp22/.cache/huggingface'
+from argparse import ArgumentParser
+from itertools import product
+import json
+import sys
+from dotenv import load_dotenv
+loaded = load_dotenv()
+if not loaded:
+    print('No .env file found')
+    sys.exit(1)
 
-from transformers import pipeline
+from src.benchmark import create_benchmark
+from src.database import BaseDatabase, JSONDatabase, MongoDB
+from src.model import create_model
+from src.evaluator import create_evaluator
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Using device: {device}')
 
-model_name = 'microsoft/phi-2'
-pipe = pipeline('text-generation', model=model_name, device_map='cuda',
-    torch_dtype=torch.float16, trust_remote_code=True)
+def benchmark(db: BaseDatabase, config: dict):
+    for (bm_cfg, model_cfg) in product(config['benchmarks'], config['models']):
+        benchmark = create_benchmark(bm_cfg)
+        model = create_model(model_cfg)
+        benchmark.run(model, db)
 
-# Generate text
-ans = pipe('To bake a cake, first', num_return_sequences=1, do_sample=True,
-    max_new_tokens=20)
-print(ans[0]['generated_text'])
+
+def evaluate(db: BaseDatabase, config: dict):
+    if 'evaluator' in config:
+        evaluators = [config['evaluator']]
+    else:
+        evaluators = config['evaluators']
+    for evaluator_cfg in evaluators:
+        evaluator = create_evaluator(evaluator_cfg)
+        for (bm_cfg, model_cfg) in product(config['benchmarks'],
+                                           config['models']):
+            benchmark = create_benchmark(bm_cfg)
+            results = benchmark.compute_results(model_cfg, db, evaluator)
+            print(results)
+
+
+def inspect(db: BaseDatabase, config: dict):
+    for (bm_cfg, model_cfg) in product(config['benchmarks'], config['models']):
+        benchmark = create_benchmark(bm_cfg)
+        benchmark.inspect_results(db, model_cfg)
+
+
+def main():
+    parser = ArgumentParser(description='Driver script for running jobs')
+    parser.add_argument('-b', '--benchmark-config',
+                        help='Path to benchmark config file')
+    parser.add_argument('-e', '--eval-config',
+                        help='Path to evaluation config file')
+    parser.add_argument('-i', '--inspect-config',
+                        help='Path to inspect config file')
+    args = parser.parse_args()
+
+    # Create the database
+    db = JSONDatabase('test_db', 'data/')
+
+    # Run the benchmark
+    if args.benchmark_config:
+        with open(f'configs/{args.benchmark_config}.json') as f:
+            bm_config = json.load(f)
+        benchmark(db, bm_config)
+
+    # Evaluate the results
+    if args.eval_config:
+        with open(f'configs/{args.eval_config}.json') as f:
+            eval_config = json.load(f)
+        evaluate(db, eval_config)
+
+    # Inspect the results
+    if args.inspect_config:
+        with open(f'configs/{args.inspect_config}.json') as f:
+            inspect_config = json.load(f)
+        inspect(db, inspect_config)
+
+
+if __name__ == '__main__':
+    main()
