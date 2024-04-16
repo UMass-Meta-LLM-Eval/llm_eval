e5f831d2f160b969d6b8917132030b81fb83eafd
Kartik Choudhary <kartikchoudh@umass.edu>
2024-04-16 15:28:55 +0000

    Added new template for MMLU
    Signed-off-by: Kartik Choudhary <kartikchoudh@umass.edu>

--- src/llm_eval/helpers/templates/__init__.py
+++ src/llm_eval/helpers/templates/__init__.py
@@ -1,2 +1,2 @@
-from .benchmark_base import BASE_SIMPLE
-from .benchmark_chat import CHAT_SIMPLE, CHAT_V2
+from .benchmark_base import BASE_SIMPLE, BASE_MMLU
+from .benchmark_chat import CHAT_SIMPLE, CHAT_V2, CHAT_MMLU
--- src/llm_eval/helpers/templates/benchmark_base.py
+++ src/llm_eval/helpers/templates/benchmark_base.py
@@ -4,3 +4,17 @@ class BASE_SIMPLE:
 
     QUESTION: str = '{fewshot}Q: {question}\nA:'
     """Simple question template for base models."""
+
+    QUESTION_ZERO_SHOT: str = 'Q: {question}\nA:'
+    """Simple zero-shot question template for base models."""
+
+
+class BASE_MMLU:
+    FEWSHOT: str = 'Question: {question}\nResponse: {answer}\n\n'
+    """Simple MMLU fewshot template for base models."""
+
+    QUESTION: str = '{fewshot}Question: {question}\nResponse:'
+    """Simple MMLU question template for base models."""
+
+    QUESTION_ZERO_SHOT: str = 'Question: {question}\nResponse:'
+    """Simple MMLU zero-shot question template for base models."""
