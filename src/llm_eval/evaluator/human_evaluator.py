fa9f63de08bc4b62b96c31b463d4cbe10608f8ef
Kartik Choudhary <kartikchoudh@umass.edu>
2024-05-02 17:25:36 +0000

    Add error analysis option for human evaluators
    Signed-off-by: Kartik Choudhary <kartikchoudh@umass.edu>

--- src/llm_eval/evaluator/human_evaluator.py
+++ src/llm_eval/evaluator/human_evaluator.py
@@ -5,12 +5,84 @@ from .base_evaluator import BaseEvaluator
 from .classic_evaluator import ExactMatchEvaluator
 from ..helpers import InfoDoc
 from ..helpers.constants.db import BENCHMARK
+from ..helpers.constants.evaluator import (VALID_HUMAN_INPUTS, POS_LABELS,
+                                           HIGH_CONFIDENCE_LABELS, ERROR_CODES)
+from ..helpers.misc import parse_error_codes
+from . import logger
 
 
-VALID_INPUTS = ['y', 'n', 'yy', 'nn', 'y?', 'n?']
+def _parse_input(user_input: str) -> tuple[bool, bool, bool, str]:
+    """Parse the user input for a human evaluator.
+    Human input should be the evaluation string. Returns a tuple with the
+    following elements:
+    - parsed: whether the input is valid
+    - success: whether the response is correct
+    - confident: whether the user is confident in their response
+    - parsing_error: an error message if the input is invalid
+    """
+    # Check if the input is valid
+    if user_input not in VALID_HUMAN_INPUTS:
+        return False, False, False, 'Invalid evaluation. Please enter one ' \
+            f'of {", ".join(f"{i}" for i in VALID_HUMAN_INPUTS)}'
+    
+    # Parse the evaluation
+    is_pos = user_input in POS_LABELS
+    is_high_confidence = user_input in HIGH_CONFIDENCE_LABELS
+    return True, is_pos, is_high_confidence, ''
 
 
-def _get_human_eval(question, response, references) -> tuple[bool, dict]:
+def _parse_input_with_error_codes(user_input: str
+                                  ) -> tuple[bool, bool, bool, list, str]:
+    """
+    Parse the user input for a human evaluator including error codes.
+    Human input should be of the form: `<pos_evaluation>` or
+    `<neg_evaluation> <error_codes>`. Returns a tuple with the following
+    elements:
+    - parsed: whether the input is valid
+    - success: whether the response is correct
+    - confident: whether the user is confident in their response
+    - error_codes: the error codes for the response
+    - parsing_error: an error message if the input is invalid"""
+
+    # Split the user input into the error evaluation and error codes
+    user_input = user_input.strip().split()
+    if len(user_input) == 0:
+        return False, False, False, [], 'No input provided'
+    
+    # Parse the evaluation
+    if user_input[0] not in VALID_HUMAN_INPUTS:
+        return False, False, False, [], 'Invalid evaluation. Please enter ' \
+            f'one of {", ".join(f"{i}" for i in VALID_HUMAN_INPUTS)}'
+    
+    is_high_confidence = user_input[0] in HIGH_CONFIDENCE_LABELS
+
+    # Return the parsed input if positive evaluation
+    if user_input[0] in POS_LABELS:
+        # In this case there should be no error codes
+        if len(user_input) > 1:
+            return False, False, False, [], 'Error codes should not be ' \
+                'provided for positive evaluations'
+        
+        # Valid positive evaluation
+        return True, True, is_high_confidence, [], ''
+    
+    # Parse the error codes
+    if len(user_input) == 1:
+        return False, False, False, [], 'Error codes not provided'
+    if len(user_input) > 2:
+        return False, False, False, [], 'Too many arguments provided'
+    
+    # Check if the error codes are valid
+    parsed_codes, unknown_codes = parse_error_codes(user_input[1])
+    if unknown_codes != '':
+        return False, False, False, [], f'Unknown error codes: {unknown_codes}'
+    
+    # Valid negative evaluation
+    return True, False, is_high_confidence, parsed_codes, ''
+
+
+def _get_human_eval(question, response, references, **kwargs
+                    ) -> tuple[bool, dict]:
     """Ask a human to evaluate the response."""
 
     # Provide the user with the question, references, and response
@@ -23,20 +95,28 @@ def _get_human_eval(question, response, references) -> tuple[bool, dict]:
             textwrap.indent(colored(response, 'green'), '  '))
     print(colored('Evaluation:', 'red'))
 
-    # Get a valid user input
-    user_input = ''
-    while user_input not in VALID_INPUTS:
-        user_input = input().lower()
-        if user_input not in VALID_INPUTS:
-            print('Invalid input. Please enter one of',
-                    {', '.join(f"'{i}'" for i in VALID_INPUTS)})
-    print()
-
-    # Return the result
-    success = user_input in ['yy','y', 'y?']
-    info = {'confident': user_input in ['yy','nn', 'y', 'n'],
-            'exact_match': False}
-    return success, info
+    # Ask the user to evaluate the response
+    input_valid = False
+    if kwargs.get('error_analysis', False):
+        while not input_valid:
+            logger.info('Please evaluate the response. In case of an error, '
+                        'valid error codes are: %s', ', '.join(
+                            f'{k} ({v})' for k, v in ERROR_CODES.items()))
+            user_input = input().lower()
+            input_valid, success, confident, error_codes, parsing_error = \
+                _parse_input_with_error_codes(user_input)
+            if not input_valid:
+                print(parsing_error)
+        return success, {'confident': confident, 'exact_match': False,
+                         'error_codes': error_codes}
+    else:
+        while not input_valid:
+            user_input = input().lower()
+            input_valid, success, confident, parsing_error = \
+                _parse_input(user_input)
+            if not input_valid:
+                print(parsing_error)
+        return success, {'confident': confident, 'exact_match': False}
 
 
 class HumanEvaluator(BaseEvaluator):
@@ -45,6 +125,7 @@ class HumanEvaluator(BaseEvaluator):
     def __init__(self, eval_config: dict):
         self._eval_config = eval_config
         self._exact_match_evaluator = ExactMatchEvaluator({})
+        self._error_analysis: bool = eval_config.get('error_analysis', False)
         self._doc = InfoDoc(**eval_config)
 
     def evaluate(self, question, response, references, **kwargs):
@@ -57,7 +138,8 @@ class HumanEvaluator(BaseEvaluator):
             return True, {'confident': True, 'exact_match': True}
 
         # Otherwise, ask a human to evaluate the response
-        return _get_human_eval(question, response, references)
+        return _get_human_eval(question, response, references,
+                               error_analysis=self._error_analysis)
 
     @property
     def config(self):
